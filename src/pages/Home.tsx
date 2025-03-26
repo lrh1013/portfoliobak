3943fce8d9e15e697a24dc40ddec36db503f0f2a
lrh1013 <dolefulmoon@gmail.com>
2025-03-26 17:13:32 +0900

    chore : 구조정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>홈입니다</h1>
+  return (
+    <h1 className="d-flex justify-content-center align-items-center vw-100 vh-100">
+      사이트 준비중입니다.
+    </h1>
+  )
 }
 
 export default Home
